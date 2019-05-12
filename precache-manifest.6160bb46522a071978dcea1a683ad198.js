3ad34b65932e17bace3abba2993993a15cdc9b30
Hassnian <hassnianxd@gmail.com>
2019-05-12 12:05:10 +0200

    Updates

---
+++ precache-manifest.6160bb46522a071978dcea1a683ad198.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5cc5b55a4753d848057b",
+    "url": "/lights-out/static/js/runtime~main.5cc5b55a.js"
+  },
+  {
+    "revision": "c2677e10a6287118d35d",
+    "url": "/lights-out/static/js/main.c2677e10.chunk.js"
+  },
+  {
+    "revision": "207e5d056e3105dcde38",
+    "url": "/lights-out/static/js/1.207e5d05.chunk.js"
+  },
+  {
+    "revision": "c2677e10a6287118d35d",
+    "url": "/lights-out/static/css/main.57fbb0a4.chunk.css"
+  },
+  {
+    "revision": "201124c27b9b6804f111e739d5f66438",
+    "url": "/lights-out/index.html"
+  }
+];
